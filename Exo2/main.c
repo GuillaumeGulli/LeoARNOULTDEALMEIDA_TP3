5a02c4a266f5fecb8b948e99fb856721a31414a2
Leo-ADA <71765962+Leo-ADA@users.noreply.github.com>
2020-09-30 15:16:59 +0200

    fin exo 2

--- Exo2/main.c
+++ Exo2/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
-	unsigned int n = 2868838400 , test;
+	unsigned int n = 2868838400;
 	int bit, i, octet;
 	octet = sizeof(n);
 	bit = octet * 8;
@@ -17,5 +17,6 @@ int main() {
 			printf("%d: OFF\n", i);
 		}
 	}
+	printf(" Bye ! ");
 	system("pause");
 }
